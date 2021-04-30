e765c12d0e688b3cd0ffa5e1bb254809855d0716
mikefab <mikefabrikant@gmail.com>
2021-04-29 21:35:56 -0500

    Add mdl

--- src/App.js
+++ src/App.js
@@ -2,17 +2,29 @@ import React from 'react';
 import { Search } from './features/search/Search';
 import { Results } from './features/results/Results';
 import { Player } from './features/player/Player';
+import Container from '@material-ui/core/Container'
+import Grid from '@material-ui/core/Grid';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Search />
-        <Player />
-        <Results />
-      </header>
-    </div>
+    <Container maxWidth="sm" >
+      <div className="App">
+        <header className="App-header">
+          <Grid container justify="center">
+            <Grid item>
+              <Search />
+            </Grid>
+            <Grid item>
+              <Player />
+            </Grid>
+            <Grid item>
+              <Results />
+            </Grid>
+          </Grid>
+        </header>
+      </div>
+    </Container>
   );
 }
 
--- src/features/search/Search.js
+++ src/features/search/Search.js
@@ -1,6 +1,10 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import styles from '../Features.module.css';
+import Button from '@material-ui/core/Button'
+import TextField from '@material-ui/core/TextField'
+import SearchIcon from '@material-ui/icons/Search'
+import Grid from '@material-ui/core/Grid';
 import {
   getResults
 } from '../results/resultsSlice';
@@ -13,23 +17,37 @@ export function Search() {
   }
 
   return (
-    <div className="container">
-      <div className={styles.row}>
-        <form onSubmit={_onSubmit}>
-          <input
-            className={styles.textbox}
-            aria-label="Type query"
-            value={query}
-            onChange={(e) => setQuery(e.target.value)}
-          />
-          <button
-            className={styles.button}
-            onClick={() => dispatch(getResults(query))}
-          >
-            Send
-          </button>
-        </form>
+      <div >
+        <div className={styles.row}>
+          <form onSubmit={_onSubmit}>
+            <Grid container spacing={2} justify="center">
+              <Grid item>
+                <TextField
+                  onKeyPress={(ev) => {
+                    console.log(`Pressed keyCode ${ev.key}`);
+                    if (ev.key === 'Enter') {
+                      // Do code here
+                      dispatch(getResults(query))
+                    }
+                  }}
+                  aria-label="Type query"
+                  value={query}
+                  variant='filled'
+                  color="primary"
+                  onChange={(e) => setQuery(e.target.value)}
+                />
+              </Grid>
+              <Grid item>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  startIcon={<SearchIcon />}
+                  onClick={() => dispatch(getResults(query))}
+                />
+              </Grid>
+            </Grid>
+          </form>
+        </div>
       </div>
-    </div>
   );
 }
