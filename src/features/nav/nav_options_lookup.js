e48d3ec6380f6b9868fa0ec01d5e840a1c649d03
mikefab <mikefabrikant@gmail.com>
2021-10-02 19:01:12 -0500

    Update nav options lookup

--- src/features/nav/nav_options_lookup.js
+++ src/features/nav/nav_options_lookup.js
@@ -1,12 +1,13 @@
 // For google translate
 export default function nav_options_lookup() {
   return {
-    'Idioms': '成语',
+    'Chengyu': '成语',
     'Duanyu': '词汇',
     'Favorites': 'Favorites',
     'History': 'History',
     'HSK': 'HSK',
     'Medias': 'Movies',
+    'Idioms': 'Idioms',
     'Prepositions': 'Prepositions',
     'Settings': 'Settings',
     'Tenses': 'Tenses',
