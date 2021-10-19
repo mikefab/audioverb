b7b0145ad8335d83d123935870c1269abc6eb15f
mikefab <mikefabrikant@gmail.com>
2021-10-18 19:16:24 -0500

    About

--- src/features/about/About.js
+++ src/features/about/About.js
@@ -4,11 +4,11 @@ export default function About() {
   return (
     <div>
       <p>
-        This app maps vocabulary, conjugations, and idiomatic expressions to foreign language movie captions. Please note that it was not developed by a linguist.
+        This app maps vocabulary, conjugations, and idiomatic expressions to foreign language movie captions.
       </p>
 <br/>
       <p>
-        <img src="/audioverb_diagram.jpg" width='450' alt='diagram'/>
+        <img src="/audioverb_diagram.jpg" width='400' alt='diagram'/>
       </p>
     </div>
   );
