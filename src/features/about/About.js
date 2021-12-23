20cf2c38f75f0b004d6a411c2dccbc09e2aa24c7
Mike Fabrikant <mikefabrikant@gmail.com>
2021-12-22 18:18:16 -0600

    screencast link

--- src/features/about/About.js
+++ src/features/about/About.js
@@ -10,6 +10,10 @@ export default function About() {
       <p>
         <img src="/audioverb_diagram.jpg" width='370' alt='diagram'/>
       </p>
+      <br/><br/>
+      <p>Check out this <a href="https://www.youtube.com/watch?v=Z-rmHxZ8pnc" target="_blank">Short screencast</a>.</p>
+
+
     </div>
   );
 }
